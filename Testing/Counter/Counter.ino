f85a7619390714da39672a63b05b210638611302
Brandon Reed <reedbj@msoe.edu>
2018-12-14 10:58:45 -0600

    Some logic change to bluetooth test app. Also added serial functionality, could be used for debugging.

--- Testing/Counter/Counter.ino
+++ Testing/Counter/Counter.ino
@@ -9,8 +9,11 @@ const char * localName = "Bluetooth Test";
 BLEPeripheral blePeriph;
 BLEService bleServ("1207");
 BLEIntCharacteristic countUpInt("1207", BLENotify);
+BLEDescriptor countUpDescriptor("2901", "Up Counter");
 BLEIntCharacteristic countDownInt("1208", BLENotify);
+BLEDescriptor countDownDescriptor("2902", "Down Counter");
 BLECharCharacteristic countSignal("1209", BLERead | BLEWrite);
+BLEDescriptor countSigDescriptor("2903", "Signal Counters");
 
 int upCnt;
 int downCnt;
@@ -42,8 +45,11 @@ void setupBLE()
 
   // Add characteristic
   blePeriph.addAttribute(countUpInt);
+  blePeriph.addAttribute(countUpDescriptor);
   blePeriph.addAttribute(countDownInt);
+  blePeriph.addAttribute(countDownDescriptor);
   blePeriph.addAttribute(countSignal);
+  blePeriph.addAttribute(countSigDescriptor);
 
   // Now that device, service, characteristic are set up,
   // initialize BLE:
@@ -60,30 +66,41 @@ void loop()
   BLECentral central = blePeriph.central();
 
   if(central) {
-    if(countSignal.written()) {
-      //If 0x30 received turn off, if 0x31 turn on
-      if(countSignal.value() == '0') {
-        cntState = false;
-      } else if(countSignal.value() == '1') {
-        cntState = true;
-      }
-    }
+    // central connected to peripheral
+    Serial.print(F("Connected to central: "));
+    Serial.println(central.address());
     
-    if(cntState) {
-      if((countUpInt.canNotify() || countUpInt.canIndicate()) && (countDownInt.canNotify()|| countDownInt.canIndicate())) {
-        //Will only work if notify and indicate are one
-        if (millis() > 1000 && (millis() - 1000) > lastTime) { 
-          // atleast one second has passed since last increment
-          // Cant use delay as it blocks bluetooth
-          lastTime = millis();
-
-          //If notifications are on, then setting the characteristics value will have the value sent
-          countUpInt.setValue(upCnt);
-          countDownInt.setValue(downCnt);
-          upCnt++;
-          downCnt--;
+    while(central.connected()) {
+      if(countSignal.written()) {
+        //If 0x30 received turn off, if 0x31 turn on
+        if(countSignal.value() == '0') {
+          cntState = false;
+          Serial.println("Stopping counters.");
+        } else if(countSignal.value() == '1') {
+          cntState = true;
+          Serial.println("Beggining counters.");
+        }
+      }
+      
+      if(cntState) {
+        if(countUpInt.subscribed() && countDownInt.subscribed()) {
+          //Will only work if notify and indicate are on/central is subscribed 
+          if (millis() > 1000 && (millis() - 1000) > lastTime) { 
+            // atleast one second has passed since last increment
+            // Cant use delay as it blocks bluetooth
+            lastTime = millis();
+  
+            //If notifications are on, then setting the characteristics value will have the value sent
+            countUpInt.setValue(upCnt);
+            countDownInt.setValue(downCnt);
+            upCnt++;
+            downCnt--;
+          }
         }
       }
     }
+    // central disconnected
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
   }
 }
